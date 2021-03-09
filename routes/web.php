2cf1bc0bc3b69ae86eb6d8041f008a85cf9fee19
Kenneth Carter <kxccarter@gmail.com>
2021-03-09 15:24:56 -0600

    authentication built

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/home', function () {
+    return view('home');
+}) -> name('home');
 
-Route::get('/register', [RegisterController:: class, 'index']);
+Route::get('/dashboard', [DashboardController:: class, 'index']) -> name('dashboard') -> middleware('auth');
+
+Route::get('/register', [RegisterController:: class, 'index']) -> name('register');
+Route::post('/register', [RegisterController:: class, 'store']);
+
+Route::get('/login', [LoginController:: class, 'index']) -> name('login');
+Route::post('/login', [LoginController:: class, 'store']);
+
+Route::post('/logout', [LogoutController:: class, 'store']) -> name('logout');
 
 Route::get('/', function () {
     return view('posts.index');
